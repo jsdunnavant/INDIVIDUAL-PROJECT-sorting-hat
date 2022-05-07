030f6d9cb25256319d08aae44ae5d2e1fde7cd90
Johnathan <jsdunnavant@gmail.com>
2022-05-06 20:50:29 -0400

    Where will we go

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-import { getStudents } from "./database.js" ;
+// import { getStudents } from "./database.js" ;
+import { addedStudent } from "./database.js";
+import { students } from "./database.js";
+// import { armyOfEvil } from "./database.js";
 
 const renderToDom = (divId, textToRender) => {
   const selectedElement = document.querySelector(divId);
-  selectedElement.innerHTML = textToRender
+  selectedElement.innerHTML = textToRender;
+};
+
+const newStudent = () => {
+  students.forEach((item, i) => {
+    item.id = i + 1;
+  });
 };
 
 const mySortHat = () => {
@@ -16,13 +25,13 @@ const mySortHat = () => {
     <p class="card-text">Are you nervous</p>
     </div>
 </div>`;
-  renderToDom('#container', domString);
+  renderToDom("#container", domString);
 };
 
-const house = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"];
-const randomHouse = Math.floor(Math.random() * house.length);
+//
+// const randomHouse = Math.floor(Math.random() * house.length);
 
-const formBtn = () => {
+const studentForm = () => {
   let domString = "";
   domString = `
   <form>
@@ -30,14 +39,14 @@ const formBtn = () => {
     <input type="text" class="form-control" id="studentName" aria-describedby="emailHelp">
   </div>
   </div>
-  <button onclick="randomHouse()" type="submit" class="btn btn-primary">Sort this out</button>
+  <button type="submit" class="btn btn-primary">Sort this out</button>
 </form>`;
-renderToDom("#sortingHat", domString);
-}
-console.log(randomHouse, house[randomHouse])
+  renderToDom("#sortingHat", domString);
+};
+
 const filterBtns = () => {
   let domString = "";
-  domString =  `
+  domString = `
   <header>Filter Students</header>
   <button type="btn btn-primary" id="ravenclaw" class="ravenclaw">Ravenclaw</button>
   <button type="btn btn-secondary" id="gryffindor" class="gryffindor">Gryffindor</button>
@@ -46,7 +55,25 @@ const filterBtns = () => {
   <button type="btn btn-danger" id="allStudents" class="allStudents">All Students</button>
   `;
   renderToDom("#filterDiv", domString);
-}
+};
+const renderToCard = (arr, divId) => {
+  let domString = `<h2>Students</h2>`;
+  for (let newStudent of arr){
+    domString += `
+    <div class="card" style="width: 18rem;">
+   <img src="..." class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">${newStudent.name}</h5>
+         <p class="card-text">${newStudent.house}</p>
+      </div>
+    </div> `;
+    renderToDom("#newCard", domString)
+  }
+};
+
+
+
+
 
 // const filterFunction = (arr) => {
 //   let domString = "";
@@ -64,32 +91,42 @@ const filterBtns = () => {
 //   renderToDom("#sortingHat", domString)
 // }
 
-
-
 // figure out filterfunction//
 const eventListeners = () => {
-  document.querySelector("#filterDiv").addEventListener('click', (e) => {
+  document.querySelector("#filterDiv").addEventListener("click", (e) => {
     if (e.target.id === "allStudents") {
-      filterFunction(students)
-    } if (e.target.id === "ravenclaw") {
-      const ravenclaw = students.filter((taco) => taco.house === "ravenclaw")
-      filterFunction(ravenclaw)
-    } if (e.target.id === "hufflepuff") {
-      const hufflepuff = students.filter((tacos) => tacos.house === "hufflepuff")
-      filterFunction(hufflepuff)
-    } if (e.target.id === "gryffindor") {
-      const gryffindor = students.filter((burrito) => burrito.house === "gryffindor")
-      filterFunction(gryffindor)
-    } if (e.target.id === "slytherin") {
-      const slytherin = students.filter((burritos) => burritos.house === "slytherin")
-      filterFunction(slytherin)
+      filterFunction(students);
     }
-  })
+    if (e.target.id === "ravenclaw") {
+      const ravenclaw = students.filter((taco) => taco.house === "ravenclaw");
+      filterFunction(ravenclaw);
+    }
+    if (e.target.id === "hufflepuff") {
+      const hufflepuff = students.filter(
+        (tacos) => tacos.house === "hufflepuff"
+      );
+      filterFunction(hufflepuff);
+    }
+    if (e.target.id === "gryffindor") {
+      const gryffindor = students.filter(
+        (burrito) => burrito.house === "gryffindor"
+      );
+      filterFunction(gryffindor);
+    }
+    if (e.target.id === "slytherin") {
+      const slytherin = students.filter(
+        (burritos) => burritos.house === "slytherin"
+      );
+      filterFunction(slytherin);
+    }
+  });
   // const student = {
   //   name: "",
   //   house: "",
-  // } 
-}
+  // }
+};
+
+
 
 //   document.querySelector("#sortingHat").addEventListener('click', (e) => {
 //     if (e.target.id === "sortingHat") {
@@ -104,20 +141,48 @@ const eventListeners = () => {
 //       name: document.querySelector('#name').value,
 //       house: document.querySelector('#house').value,
 //     };
-  
+
 //      students.push(newStudentCard);
 //      filterFunction();
 //      formModal.hide();
 //      form.reset();
 //   });
 
+const assignHouse = () => {
+  const houses = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"];
+  document.querySelector("#studentName").addEventListener("submit", (e) => {
+    e.preventDefault();
+    // const newStudentId = () => {
+    //   students.forEach((item, i) =>{
+    //   item.id = i + 1;
+    //  })
+    // }
+    const addStudent = {
+      id: students[students.length - 1].id + 1,
+
+      name: document.querySelector("#studentName".value),
+      house: Math.floor(Math.random() * houses.length),
+    };
+    addedStudent.push(addStudent);
+    students.push(addStudent);
+    document.querySelector("#studentName").reset();
+    renderToCard(addedStudent, "#newHouse")
+  });
+  // console.log(assignHouse());
+};
+// id: students.forEach((item, i) =>{
+//   //     item.id = i + 1;
+//   //    }),
+
 const startApp = () => {
+  newStudent();
   mySortHat();
   // filterFunction(students);
-
-  formBtn();
+  studentForm();
   filterBtns();
+  renderToCard();
   eventListeners();
+  assignHouse();
 };
 
 startApp();
